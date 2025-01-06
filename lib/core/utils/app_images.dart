783fbfff75a79fb7f7b85193e6e02ef158042dd0
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-01-06 11:14:50 +0200

    Update app icons, splash screens, and styles
    - Removed the inclusion of `flutter_lints` in `analysis_options.yaml`.
    - Added new app icon images for various resolutions.
    - Updated `launch_background.xml` and `LaunchScreen.storyboard` to use a new background image.
    - Added new styles for splash screens in `styles.xml` and `LaunchScreen.storyboard`.
    - Configured `flutter_launcher_icons` and `flutter_native_splash` in `pubspec.yaml`.
    - Added `app_images.dart` to manage image assets.
    - Updated `web/index.html` to include a splash screen style and script.
    - Modified `Info.plist` to include `UIStatusBarHidden` key.
    - Updated `pubspec.lock` with new dependencies.

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesIcLauncher
+  /// assets/images/ic_launcher.png
+  static const String imagesIcLauncher = "assets/images/ic_launcher.png";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.png
+  static const String imagesLogo = "assets/images/logo.png";
+
+  /// Assets for imagesLogoErased
+  /// assets/images/logoErased.png
+  static const String imagesLogoErased = "assets/images/logoErased.png";
+
+  /// Assets for imagesLogoWithoutWords
+  /// assets/images/logoWithoutWords.png
+  static const String imagesLogoWithoutWords = "assets/images/logoWithoutWords.png";
+}
+
