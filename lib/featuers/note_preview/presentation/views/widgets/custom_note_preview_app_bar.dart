6e870eb6b93dc921fc8e8e6121f07c40cee0a48f
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-01-06 14:51:17 +0200

    Add delete confirmation dialog and enhance note preview body
    Implemented a delete confirmation dialog in the `CustomNotePreviewAppBar` widget. Also, enhanced the `NotePreviewBody` widget by adding styled text for the note title, date, and content.

--- lib/featuers/note_preview/presentation/views/widgets/custom_note_preview_app_bar.dart
+++ lib/featuers/note_preview/presentation/views/widgets/custom_note_preview_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notiqo/core/utils/app_styles.dart';
 import 'package:notiqo/core/widgets/custom_app_bar_container.dart';
 
 class CustomNotePreviewAppBar extends StatelessWidget {
@@ -16,7 +17,43 @@ class CustomNotePreviewAppBar extends StatelessWidget {
         ),
         Spacer(),
         CustomAppBarContainer(
-          onTap: () {},
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text(
+                    'Delete Note',
+                    style: TextStyles.font22Regular(context),
+                  ),
+                  content: Text(
+                    'Are you sure you want to delete this note?',
+                    style: TextStyles.font18Regular(context),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        'Cancel',
+                        style: TextStyles.font18Regular(context),
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          'Delete',
+                          style: TextStyles.font18Regular(context)
+                              .copyWith(color: Colors.red),
+                        )),
+                  ],
+                );
+              },
+            );
+          },
           iconData: Icons.delete,
         ),
         const SizedBox(
--- lib/featuers/note_preview/presentation/views/widgets/note_preview_body.dart
+++ lib/featuers/note_preview/presentation/views/widgets/note_preview_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notiqo/core/utils/app_colors.dart';
+import 'package:notiqo/core/utils/app_styles.dart';
 import 'custom_note_preview_app_bar.dart';
 
 class NotePreviewBody extends StatelessWidget {
@@ -15,7 +17,43 @@ class NotePreviewBody extends StatelessWidget {
           ),
           SliverToBoxAdapter(
             child: CustomNotePreviewAppBar(),
-          )
+          ),
+          SliverToBoxAdapter(
+            child: const SizedBox(
+              height: 20,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Text(
+              '10 Excellent font packages for Flutter developers',
+              style: TextStyles.font24SemiBold(context)
+                  .copyWith(color: AppColors.mainColorOfText, fontSize: 28),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: const SizedBox(
+              height: 20,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Text(
+              'May 21,2025',
+              style: TextStyles.font18Medium(context)
+                  .copyWith(color: AppColors.secondaryColorOfText),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: const SizedBox(
+              height: 10,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Text(
+              'The poor girl bore all patiently, and dared not tell her father, who would have rattled her off; for his wife governed him entirely. When she had done her work, she used to go into the chimney-corner, and sit down among cinders and ashes, which made her commonly be called Cinderwench; but the youngest, who was not so rude and uncivil as the eldest, called her Cinderella. However, Cinderella, notwithstanding her mean apparel, was a hundred times handsomer than her sisters, though they were always dressed very richly.It happened that the King\'s son gave a ball, and invited all persons of fashion to it. Our young misses were also invited, for they cut a very grand figure among the quality. They were mightily delighted at this invitation, and wonderfully busy in choosing out such gowns, petticoats, and head-clothes as might become them. This was a new trouble to Cinderella; for it was she who ironed her sisters\' linen, and plaited their ruffles; they talked all day long of nothing but how they should be dressed.',
+              style: TextStyles.font20Regular(context)
+                  .copyWith(color: AppColors.mainColorOfText),
+            ),
+          ),
         ],
       ),
     );
