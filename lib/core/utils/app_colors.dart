03bde3e49e47a56625ced519a60666e7cfc60100
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-01-06 10:55:18 +0200

    Add Poppins fonts and logo images, update app styles and colors, and remove widget test
    Added Poppins font files in various weights and updated the `pubspec.yaml` to include these fonts. Also added logo images to the assets. Created `app_colors.dart` for color definitions and `app_styles.dart` for text styles. Removed the default widget test file.

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color mainColorTheme = Colors.black;
+  static const Color secondaryColorTheme = Color(0xff29282B);
+  static const Color mainColorOfText = Colors.white;
+  static const Color secondaryColorOfText = Color(0xff666666);
+}
