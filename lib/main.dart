7f8a317761e78149a4a0eb7e31533a7639ba697e
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-01-08 10:57:57 +0200

    Add NoteModel and integrate Hive for local storage
    - Created `NoteModel` class for note data with Hive annotations.
    - Generated `NoteModelAdapter` for Hive serialization.
    - Updated `main.dart` to initialize Hive and register the adapter.
    - Added necessary Hive dependencies to `pubspec.yaml` and `pubspec.lock`.
    - Updated `GeneratedPluginRegistrant.swift` to include PathProviderPlugin.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notiqo/core/models/note_model.dart';
 import 'package:notiqo/featuers/splash/presentation/views/splash_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>('notes');
   runApp(DevicePreview(enabled: true, builder: (context) => const NotiqoApp()));
 }
 
